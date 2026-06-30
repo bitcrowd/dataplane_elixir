abaf83d2331e30641f22f722fc9d63873328a1cf
Joel Koch <joel@bitcrowd.net>
2026-06-30 15:14:20 +0200

    remove repo, write repo, progress

--- lib/dataplane_ex/write_repo.ex
+++
@@ -1,7 +0,0 @@
-defmodule DataplaneEx.WriteRepo do
-  @moduledoc """
-  Write-side Ecto repository for Postgres mutations.
-  """
-
-  use Ecto.Repo, otp_app: :dataplane_ex, adapter: Ecto.Adapters.Postgres
-end
