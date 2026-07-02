1c7cf9d42dad209db2858bd8dc15356bd74d1fcc
Alfonso Manuel <almancill@gmail.com>
2026-07-02 12:07:39 +0200

    Revert "Dockerize"

--- config/runtime.exs
+++ config/runtime.exs
@@ -1,5 +1,21 @@
 import Config
 
+# config/runtime.exs is executed for all environments, including
+# during releases. It is executed after compilation and before the
+# system starts, so it is typically used to load production configuration
+# and secrets from environment variables or elsewhere. Do not define
+# any compile-time configuration in here, as it won't be applied.
+# The block below contains prod specific runtime configuration.
+
+# ## Using releases
+#
+# If you use `mix release`, you need to explicitly enable the server
+# by passing the PHX_SERVER=true when you start it:
+#
+#     PHX_SERVER=true bin/dataplane_ex start
+#
+# Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
+# script that automatically sets the env var above.
 if System.get_env("PHX_SERVER") do
   config :dataplane_ex, DataplaneExWeb.Endpoint, server: true
 end
@@ -33,13 +49,46 @@ if config_env() == :prod do
 
   config :dataplane_ex, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
-  {:ok, bind_ip} =
-    System.get_env("BSKY_DATAPLANE_IP", "127.0.0.1")
-    |> String.to_charlist()
-    |> :inet.parse_address()
-
   config :dataplane_ex, DataplaneExWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
-    http: [ip: bind_ip],
+    http: [
+      # Enable IPv6 and bind on all interfaces.
+      # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
+      # See the documentation on https://hexdocs.pm/bandit/Bandit.html#t:options/0
+      # for details about using IPv6 vs IPv4 and loopback vs public addresses.
+      ip: {0, 0, 0, 0, 0, 0, 0, 0}
+    ],
     secret_key_base: secret_key_base
+
+  # ## SSL Support
+  #
+  # To get SSL working, you will need to add the `https` key
+  # to your endpoint configuration:
+  #
+  #     config :dataplane_ex, DataplaneExWeb.Endpoint,
+  #       https: [
+  #         ...,
+  #         port: 443,
+  #         cipher_suite: :strong,
+  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
+  #       ]
+  #
+  # The `cipher_suite` is set to `:strong` to support only the
+  # latest and more secure SSL ciphers. This means old browsers
+  # and clients may not be supported. You can set it to
+  # `:compatible` for wider support.
+  #
+  # `:keyfile` and `:certfile` expect an absolute path to the key
+  # and cert in disk or a relative path inside priv, for example
+  # "priv/ssl/server.key". For all supported SSL configuration
+  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
+  #
+  # We also recommend setting `force_ssl` in your config/prod.exs,
+  # ensuring no data is ever sent via http, always redirecting to https:
+  #
+  #     config :dataplane_ex, DataplaneExWeb.Endpoint,
+  #       force_ssl: [hsts: true]
+  #
+  # Check `Plug.SSL` for all available options in `force_ssl`.
 end
