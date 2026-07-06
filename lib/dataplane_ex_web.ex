68fd3be380524ab279ff2f851ea9a725cfe6ebc2
Joel Koch <joel@bitcrowd.net>
2026-07-06 11:26:26 +0200

    Remove frontend leftovers

--- lib/dataplane_ex_web.ex
+++ lib/dataplane_ex_web.ex
@@ -6,7 +6,6 @@ defmodule DataplaneExWeb do
   This can be used in your application as:
 
       use DataplaneExWeb, :controller
-      use DataplaneExWeb, :html
 
   The definitions below will be executed for every controller,
   component, etc, so keep them short and clean, focused
@@ -17,76 +16,22 @@ defmodule DataplaneExWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
-
   def router do
     quote do
       use Phoenix.Router, helpers: false
 
-      # Import common connection and controller functions to use in pipelines
       import Plug.Conn
       import Phoenix.Controller
       import Phoenix.LiveView.Router
     end
   end
 
-  def channel do
-    quote do
-      use Phoenix.Channel
-    end
-  end
-
   def controller do
     quote do
-      use Phoenix.Controller, formats: [:html, :json]
+      use Phoenix.Controller, formats: [:json]
 
       import Plug.Conn
 
-      unquote(verified_routes())
-    end
-  end
-
-  def live_view do
-    quote do
-      use Phoenix.LiveView
-
-      unquote(html_helpers())
-    end
-  end
-
-  def live_component do
-    quote do
-      use Phoenix.LiveComponent
-
-      unquote(html_helpers())
-    end
-  end
-
-  def html do
-    quote do
-      use Phoenix.Component
-
-      # Import convenience functions from controllers
-      import Phoenix.Controller,
-        only: [get_csrf_token: 0, view_module: 1, view_template: 1]
-
-      # Include general helpers for rendering HTML
-      unquote(html_helpers())
-    end
-  end
-
-  defp html_helpers do
-    quote do
-      # HTML escaping functionality
-      import Phoenix.HTML
-      # Core UI components
-      import DataplaneExWeb.CoreComponents
-
-      # Common modules used in templates
-      alias Phoenix.LiveView.JS
-      alias DataplaneExWeb.Layouts
-
-      # Routes generation with the ~p sigil
       unquote(verified_routes())
     end
   end
@@ -96,13 +41,10 @@ defmodule DataplaneExWeb do
       use Phoenix.VerifiedRoutes,
         endpoint: DataplaneExWeb.Endpoint,
         router: DataplaneExWeb.Router,
-        statics: DataplaneExWeb.static_paths()
+        statics: []
     end
   end
 
-  @doc """
-  When used, dispatch to the appropriate controller/live_view/etc.
-  """
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
   end
